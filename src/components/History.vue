3d7f4c92e46cf1af63575de23b3aecaa9318df22
Buzzology <chrisowens321@gmail.com>
2021-07-21 17:11:34 +1000

    [ADD] Completed tutorial

---
+++ src/components/History.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h4>History</h4>
+    <div class="flex">
+      <p v-for="(item, index) in history" :key="index" :class="activeIndexes(Number(value)).includes(index) && 'bold'">
+        {{ item }}
+      </p>
+    </div>
+    <input placeholder="Seach by Index" type="number" v-model="value" />
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      value: 0,
+    };
+  },
+  components: {},
+  computed: {
+    ...mapState(["history"]),
+    ...mapGetters(["activeIndexes"]),
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  margin-top: 7rem;
+}
+
+.flex {
+  display: flex;
+  text-align: center;
+  width: 25rem;
+  margin: 0 auto;
+  flex-wrap: wrap;
+}
+
+.flex p {
+  margin: 1rem;
+}
+
+.bold {
+  font-weight: 900;
+}
+</style>
